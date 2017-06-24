56f0106491246d4df05f7689a7118d5e18fbb257
robiso <r@robiso.com>
2017-06-24 19:31:44 +0200

    Update script.js
    Space fix.

--- plugins/addition_contents/js/script.js
+++ plugins/addition_contents/js/script.js
@@ -24,7 +24,7 @@ $(function() {
 				target: 'pages',
 				token: token,
 			});
-		}else{
+		} else{
 			var id = 'addition_content_'+$(this).attr('value');
 			$.ajaxSetup({async: false});
 			$.post("",{
