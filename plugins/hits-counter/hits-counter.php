2a0f1a298c61482bbb5ae80fb87d46f32be15540
Robert Isoski <r@robiso.com>
2019-04-07 19:18:12 +0200

    Update hits-counter.php
    Updating for future version (WonderCMS 3.0.0).

--- plugins/hits-counter/hits-counter.php
+++ plugins/hits-counter/hits-counter.php
@@ -5,19 +5,22 @@
  * Simple hits/visits counter. Hits are displayed in the footer once the admin is logged in.
  * Hits will not be incremented if admin is logged in.
  *
- * @author Yassine Addi <yassineaddi.dev@gmail.com> // edit by robiso
- * @version 2.4 // edit by robiso
+ * @author Yassine Addi <yassineaddi.dev@gmail.com> // Forked by Robert Isoski
+ * @version 3.0
  */
+
+global $Wcms;
 
 if (defined('VERSION') && !defined('version')) {
 	define('version', VERSION);
 }
 
-wCMS::addListener('menu', 'incrementHits');
-wCMS::addListener('footer', 'displayHits');
+$Wcms->addListener('menu', 'incrementHits');
+$Wcms->addListener('footer', 'displayHits');
 
 function incrementHits ($args) {
-	if (wCMS::$loggedIn) return $args;
+    global $Wcms;
+	if ($Wcms->loggedIn) return $args;
 	$hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
 	if ( ! isset($_SESSION['_wcms_hits_counter'])) {
 		$_SESSION['_wcms_hits_counter'] = time();
@@ -31,7 +34,8 @@ function incrementHits ($args) {
 }
 
 function displayHits ($args) {
-	if ( ! wCMS::$loggedIn) return $args;
+    global $Wcms;
+	if ( ! $Wcms->loggedIn) return $args;
 	$hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
 	$args[0] .= ' &bull; Hits: ' . $hits;
 	return $args;
