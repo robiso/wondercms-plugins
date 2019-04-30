ff87a9551dee875cd653714527138d8660a90816
Robert Isoski <r@robiso.com>
2019-04-30 05:01:46 +0200

    Update hits-counter.php
    Add fix by @notcarbide + minor code prettification.

--- plugins/hits-counter/hits-counter.php
+++ plugins/hits-counter/hits-counter.php
@@ -2,18 +2,19 @@
 /**
  * Hits counter.
  *
- * Simple hits/visits counter. Hits are displayed in the footer once the admin is logged in.
- * Hits will not be incremented if admin is logged in.
+ * Simple hits/visits counter. Hits are displayed in the footer only to the admin.
+ * Hits are  not incremented if admin is logged in.
  *
- * @author Yassine Addi <yassineaddi.dev@gmail.com> // Forked by Robert Isoski
- * @version 3.0
+ * @author Yassine Addi <yassineaddi.dev@gmail.com>
+ * Forked by Robert Isoski
+ * @version 3.0.0
  */
 
 global $Wcms;
 
-
-if (defined('VERSION') && !defined('version')) {
-	define('version', VERSION);
+if (defined('VERSION')) {
+    define('version', VERSION);
+    defined('version') OR die('Direct access is not allowed.');
 }
 
 $Wcms->addListener('menu', 'incrementHits');
@@ -21,23 +22,30 @@ $Wcms->addListener('footer', 'displayHits');
 
 function incrementHits ($args) {
     global $Wcms;
-	if ($Wcms->loggedIn) return $args;
-	$hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
-	if ( ! isset($_SESSION['_wcms_hits_counter'])) {
-		$_SESSION['_wcms_hits_counter'] = time();
-		$hits++;
-	}
-	if ((time()-$_SESSION['_wcms_hits_counter'])>600)
-		$hits++;
-	$_SESSION['_wcms_hits_counter'] = time();
-	file_put_contents(__DIR__ . '/hits.txt', $hits);
-	return $args;
+
+    if ($Wcms->loggedIn) {
+        return $args;
+    }
+    $hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
+    if ( ! isset($_SESSION['_wcms_hits_counter'])) {
+        $_SESSION['_wcms_hits_counter'] = time();
+        $hits++;
+    }
+    if ((time()-$_SESSION['_wcms_hits_counter'])>600) {
+        $hits++;
+    }
+    $_SESSION['_wcms_hits_counter'] = time();
+    file_put_contents(__DIR__ . '/hits.txt', $hits);
+    return $args;
 }
 
 function displayHits ($args) {
     global $Wcms;
-	if ( ! $Wcms->loggedIn) return $args;
-	$hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
-	$args[0] .= ' &bull; Hits: ' . $hits;
-	return $args;
+    
+    if ( ! $Wcms->loggedIn) {
+        return $args;
+    }
+    $hits = file_exists(__DIR__ . '/hits.txt') ? (int) file_get_contents(__DIR__ . '/hits.txt') : 0;
+    $args[0] .= ' &bull; Hits: ' . $hits;
+    return $args;
 }
